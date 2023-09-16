3fa45686aa019533abd4bbf29432a6423855e4d1
Burak Hamza <thedatcal@gmail.com>
2023-09-16 19:08:10 +0200

    initial commit 2

--- deploy.php
+++ deploy.php
@@ -3,9 +3,18 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
-host('prod')
+set('repository','git@github.com:datcal/steam-app.git');
+
+// env file
+
+
+set('shared_files', ['.env']);
+
+
+
+host('burak.fun')
     ->set('remote_user', 'root')
-    ->set('hostname', '139.162.190.96')
+    ->set('hostname', '139.144.183.99')
     ->set('deploy_path', '/var/www/burak.fun/steam');
 
 // Hooks
